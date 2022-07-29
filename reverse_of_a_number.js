89881a8c4a68d3e064ba193eaacfbf9087932e82
Ankursoni23 <97575335+Ankursoni23@users.noreply.github.com>
2022-07-29 16:15:07 +0530

    Update reverse_of_a_number.js

--- reverse_of_a_number.js
+++ reverse_of_a_number.js
@@ -1,6 +1,6 @@
-function reverse_a_number(n)
+function reverse_of_a_number(n)
 {
 	n = n + "";
 	return n.split("").reverse().join("");
 }
-console.log(Number(reverse_a_number(32243)));
+console.log(Number(reverse_of_a_number(149)));
